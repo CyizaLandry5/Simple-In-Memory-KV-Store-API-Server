ab5b4d33677042289d5445d3388fec4e4bfba067
CyizaLandry5 <103103265+CyizaLandry5@users.noreply.github.com>
2026-06-25 05:23:52 +0200

    const prefix

--- 20260625_4d81f6.ts
+++ 20260625_4d81f6.ts
@@ -34,3 +34,5 @@ app.get("/kv/list/:prefix", async (c) => {
   if (token !== "2609_15e4c3:9ca6a5") {
     return c.text("Missing or invalid token", 401);
   }
+  const prefix = c.req.param("prefix");
+  const records = [];
